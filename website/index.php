b7827521c76c41e1b6a19f671efe2e4b21451419
Tryaxor <43273854+Tryaxor@users.noreply.github.com>
2019-03-12 17:33:21 +0100

    Add files via upload

--- website/index.php
+++ website/index.php
@@ -1,16 +1,58 @@
-<?php
-function getTranslation($section, $lang){
-  return "result of the query in that language";
-}
-?>
 <!DOCTYPE html>
-<html>
-  <head>
-  </head>
-  <body>
-    <h1>The website</h1>
-    <div>
-      <?php echo getTranslation("intro", "fr"); ?> // the introduction in french for example.
-    </div>
-  </body>
+<html lang="en">
+<?php
+            include("head.inc");
+            //$translations = simplexml_load_file("translation.xml");
+            ?>
+
+<body>
+    
+    <?php
+            include("nav.inc");
+            ?>
+
+
+    <main>
+        <article>
+            <header>
+                <h2><?getTranslation("title1", $lang)?></h2>
+                <p></p>
+            </header>
+            <div class="article_main">
+                <h3><?getTranslation("title1_1", $lang);?></h3>
+                
+                <p>><?getTranslation("txt1_1", $lang);?></p>
+            </div>
+            <footer>
+                <p><?getTranslation("footer1", $lang);?></p>
+            </footer>
+        </article>
+        
+        <article>
+            <header>
+                <h2><?getTranslation("title2", $lang)?></h2>
+                <p></p>
+            </header>
+            <div class="article_main">
+                <h3><?getTranslation("title2_1", $lang)?></h3>
+                
+                <p><?getTranslation("txt2_1", $lang);?></p>
+                
+                <h3><?getTranslation("title2_2", $lang)?></h3>
+                
+                <p><?getTranslation("txt2_2", $lang);?></p>
+            </div>
+            <footer>
+                <p><?getTranslation("footer2", $lang);?></p>
+            </footer>
+        </article>
+
+    </main>
+    <footer>
+        <?php
+            include("footer.inc");
+            ?>
+    </footer>
+</body>
+
 </html>
