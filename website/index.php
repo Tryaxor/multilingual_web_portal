8c69a8e77307b5083327ac30d05d6e84ebe94295
Tryaxor <43273854+Tryaxor@users.noreply.github.com>
2019-03-16 15:51:48 +0100

    Add files via upload

--- website/index.php
+++ website/index.php
@@ -1,49 +1,108 @@
-<!DOCTYPE html>
-<html lang="en">
 <?php
+        include("connection.inc");
             include("head.inc");
             //$translations = simplexml_load_file("translation.xml");
-            ?>
+?>
+
+<!DOCTYPE html>
+<html lang="en">
 
 <body>
-    
+
     <?php
             include("nav.inc");
-            ?>
-
-
+    ?>
     <main>
+        <div class="fullWidth" id="pic1">
+            
+        </div>
         <article>
             <header>
-                <h2><?getTranslation("title1")?></h2>
+            <h2>All users (not secure at all, just debugging)</h2>
+            </header>
+            <div class="article_main">
+                <p>use phpMyadmin to reset.</p>
+        <p>
+            <?php
+        $select_user = $db->prepare("SELECT * FROM login_credentials;");
+        $select_user->execute();
+        echo("<br/>");
+        while($user = $select_user->fetch()){
+            echo " <br/>id : $user[0]";
+            echo " <br/>username : $user[1]";
+            echo " <br/>pass hash : $user[2]";
+            echo " <br/>first name : $user[3]";
+            echo " <br/>last name : $user[4]";
+            echo " <br/>_______________________";
+    }
+        ?>
+        </p>
+            </div>
+        </article>
+
+        <article>
+            <header><h2>LOG IN</h2></header>
+            <div class="article_main">
+                <p>
+                    <?
+                    echo $msg;
+                    ?>
+                </p>
+            </div>
+        </article>
+
+
+        <article>
+            <header>
+                <h2>
+                    <?getTranslation("title1")?>
+                </h2>
                 <p></p>
             </header>
             <div class="article_main">
-                <h3><?getTranslation("title1_1");?></h3>
-                
-                <p><?getTranslation("txt1_1");?></p>
+                <h3>
+                    <?getTranslation("title1_1");?>
+                </h3>
+
+                <p>
+                    <?getTranslation("txt1_1");?>
+                </p>
             </div>
             <footer>
-                <p><?getTranslation("footer1");?></p>
+                <p>
+                    <?getTranslation("footer1");?>
+                </p>
             </footer>
         </article>
-        
+
         <article>
             <header>
-                <h2><?getTranslation("title2")?></h2>
+                <h2>
+                    <?getTranslation("title2")?>
+                </h2>
                 <p></p>
             </header>
             <div class="article_main">
-                <h3><?getTranslation("title2_1")?></h3>
-                
-                <p><?getTranslation("txt2_1");?></p>
-                
-                <h3><?getTranslation("title2_2")?></h3>
-                
-                <p><?getTranslation("txt2_2");?></p>
+                <h3>
+                    <?getTranslation("title2_1")?>
+                </h3>
+
+                <p>
+                    <?getTranslation("txt2_1");?>
+                </p>
+
+                <h3>
+                    <?getTranslation("title2_2")?>
+                </h3>
+
+                <p>
+                    <?getTranslation("txt2_2");?>
+                </p>
             </div>
             <footer>
-                <p><?getTranslation("footer2");?></p>
+                <p>
+                    <?getTranslation("footer2");?>
+                </p>
             </footer>
         </article>
 
